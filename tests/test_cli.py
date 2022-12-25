cb230f332b2edb0be972989ad8d59a81972bddea
Tugrul Ates <me@tugrulates.com>
2022-12-25 13:22:23 +0000

    Use simpler fixture name in tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -12,47 +12,47 @@ runner = CliRunner()
 
 
 @pytest.fixture
-def test_config(tmp_path: Path) -> cli.Config:
+def config(tmp_path: Path) -> cli.Config:
     """Fixture for config."""
     cli.config = cli.Config(tmp_path / "config.json")
     cli.config.dump()
     return cli.config
 
 
-def test_help(test_config: cli.Config) -> None:
+def test_help(config: cli.Config) -> None:
     """Test help."""
-    test_config.load()
+    config.load()
     result = runner.invoke(cli.app, ["--help"])
     assert result.exit_code == 0
     assert "Usage:" in result.stdout
 
 
-def test_quote(test_config: cli.Config):
+def test_quote(config: cli.Config):
     """Test Zoolander quote."""
-    test_config.load()
+    config.load()
     result = runner.invoke(cli.app)
     assert result.exit_code == 0
     assert len(result.stdout.strip().split("\n")) == 1
 
 
-def test_favorite_quote(test_config: cli.Config):
+def test_favorite_quote(config: cli.Config):
     """Test Zoolander quote."""
-    test_config.load()
-    print(test_config.path)
+    config.load()
+    print(config.path)
     result = runner.invoke(cli.app, ["--favorite=break-dance"])
-    print(test_config.prefs)
+    print(config.prefs)
     assert result.exit_code == 0
     assert result.stdout.strip() == "They're break-dance fighting."
-    print(test_config.path)
-    print(test_config.prefs)
+    print(config.path)
+    print(config.prefs)
     result = runner.invoke(cli.app)
     assert result.exit_code == 0
     assert result.stdout.strip() == "They're break-dance fighting."
 
 
-def test_favorite_quote_missing(test_config: cli.Config):
+def test_favorite_quote_missing(config: cli.Config):
     """Test Zoolander quote."""
-    test_config.load()
+    config.load()
     result = runner.invoke(cli.app, ["--favorite=derek"])
     assert result.exit_code == 0
     assert result.stdout.strip() == "You've done nothing! NOTHIIIING!"

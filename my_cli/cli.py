722f6f539605be45a8686e20efe4b436ce690a7f
Tugrul Ates <me@tugrulates.com>
2022-12-25 21:49:27 +0000

    Remove config persistance
    It is too convoluted for a sample project.

--- my_cli/cli.py
+++ my_cli/cli.py
@@ -7,27 +7,15 @@ from typing import Optional
 
 import typer
 
-from my_cli.config import Config
-
 APP_NAME = "my-cli"  # change this
 
-
-# Persistent user config
-config = Config(Path(typer.get_app_dir(APP_NAME)) / "config.json")
-config.load()
 
 # App object enclosing the commands.
 app = typer.Typer(help=__doc__)
 
-# An option that sync its values to the config file.
-# $ my-cli --favorite=hansel  # write 'favorite=value' to config file
-# $ my-cli                    # reuse from config file
-# $ my-cli --favorite=        # reset favorite
-FavoriteOption = config.option("favorite", help="Favorite quote.")
-
 
 @app.command()
-def quote(favorite: Optional[str] = FavoriteOption) -> None:
+def quote(favorite: Optional[str] = None) -> None:
     """Print a random quote from Zoolander."""
     # Static files will be in site-packages next to this file.
     quotes_path = Path(__file__).parent / "static" / "my_quotes.txt"
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -11,48 +11,29 @@ from my_cli import cli
 runner = CliRunner()
 
 
-@pytest.fixture
-def config(tmp_path: Path) -> cli.Config:
-    """Fixture for config."""
-    cli.config = cli.Config(tmp_path / "config.json")
-    cli.config.dump()
-    return cli.config
-
-
-def test_help(config: cli.Config) -> None:
+def test_help() -> None:
     """Test help."""
-    config.load()
     result = runner.invoke(cli.app, ["--help"])
     assert result.exit_code == 0
     assert "Usage:" in result.stdout
 
 
-def test_quote(config: cli.Config):
+def test_quote() -> None:
     """Test Zoolander quote."""
-    config.load()
     result = runner.invoke(cli.app)
     assert result.exit_code == 0
     assert len(result.stdout.strip().split("\n")) == 1
 
 
-def test_favorite_quote(config: cli.Config):
+def test_favorite_quote() -> None:
     """Test Zoolander quote."""
-    config.load()
-    print(config.path)
     result = runner.invoke(cli.app, ["--favorite=break-dance"])
-    print(config.prefs)
-    assert result.exit_code == 0
-    assert result.stdout.strip() == "They're break-dance fighting."
-    print(config.path)
-    print(config.prefs)
-    result = runner.invoke(cli.app)
     assert result.exit_code == 0
     assert result.stdout.strip() == "They're break-dance fighting."
 
 
-def test_favorite_quote_missing(config: cli.Config):
+def test_favorite_quote_missing() -> None:
     """Test Zoolander quote."""
-    config.load()
     result = runner.invoke(cli.app, ["--favorite=derek"])
     assert result.exit_code == 0
     assert result.stdout.strip() == "You've done nothing! NOTHIIIING!"

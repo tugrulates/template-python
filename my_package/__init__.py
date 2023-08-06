83b9d5fde4e4d71b976bcdcc3fd568e61210a8d5
Tugrul Ates <me@tugrulates.com>
2023-08-06 11:42:34 +0300

    fix typing for older versions

--- my_package/__init__.py
+++ my_package/__init__.py
@@ -6,9 +6,10 @@ Non-CLI code for the package can live here.
 
 
 from pathlib import Path
+from typing import List
 
 
-def get_quotes() -> list[str]:
+def get_quotes() -> List[str]:
     """Return the list of quotes from the static quotes file."""
     # Static files will be in site-packages next to this file.
     quotes_path = Path(__file__).parent / "static" / "my_quotes.txt"
